0b8d7d9d5354366b0b88434a4e8777ac3de101be
fadhil <fadhil.bagi@gmail.com>
2023-09-30 00:07:03 +0700

    adding action bar for player

--- Battleground.gd
+++ Battleground.gd
@@ -1,16 +1,28 @@
 extends Node2D
 @export var mob_scene: PackedScene
+@export var player_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().call_group(&"mobs", &"queue_free")
-	$Player.start($Player1Start.position)
+	get_tree().call_group(&"playes", &"queue_free")
+	
+	var player = player_scene.instantiate()
+	var player_spawn_location = $Player1Start.position
+	player.position = player_spawn_location
+	add_child(player)
+	player.action.connect(_on_action)
 	
 	var mob = mob_scene.instantiate()
 	var mob_spawn_location = $Enemy1Start.position
 	mob.position = mob_spawn_location
 	add_child(mob)
+	$SpeedTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_action():
+	$SpeedTimer.stop()
--- Player.gd
+++ Player.gd
@@ -1,8 +1,15 @@
 extends Node2D
+var health = 25
+var speed = 128
+var attack = 4
+var velocity = speed
 
+signal action
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$ActionBar.value = 0
+	get_parent().get_node("SpeedTimer").connect("timeout",startGame)
 	pass # Replace with function body.
 
 
@@ -13,3 +20,10 @@ func _process(delta):
 func start(pos):
 	position = pos
 	show()
+
+func startGame():
+	var value = $ActionBar.value
+	if(value < $ActionBar.max_value) : 
+		$ActionBar.value += velocity
+	else :
+		action.emit()
